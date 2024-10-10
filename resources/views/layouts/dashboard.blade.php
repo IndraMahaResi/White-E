9bb302a3cd26ca6057ad11bf7315256c3a083073
unknown <indramaha.resi@gmail.com>
2024-10-10 22:27:23 +0700

    Halaman Dashboard

---
+++ resources/views/layouts/dashboard.blade.php
@@ -0,0 +1,8 @@
+<!-- resources/views/dashboard.blade.php -->
+@extends('layouts.dashboard')
+
+@section('content')
+    <!-- Content for the dashboard page will go here -->
+     <!-- resources/views/layouts/dashboard.blade.php -->
+
+@endsection
